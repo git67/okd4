a9df76a02287b0f2339115e7b96c8d7137ed10da
Heiko Stein <hs@heiko-stein.de>
2021-04-25 20:38:52 +0200

    fix mk_inst.sh

--- mk_inst.sh
+++ mk_inst.sh
@@ -1,12 +1,20 @@
 #!/bin/bash 
+#
+# ident "@(#)<mk_inst.sh> <0.1.0>"
+#
+# desc          : <Vorbereitung OKD Setup"
+# version       : <0.1.0>
+# dev           : <heiko.stein@etomer.com>
 
 set -euo pipefail
+PATH="${PATH}:/:/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/bin"
 
 PXE_OKD4_DIR="/var/lib/tftpboot/okd4"
 WEB_DIR="/var/www/html"
 WWW_OKD4_DIR="${WEB_DIR}/okd4/"
 
 BASE_DIR="/data/okd4"
+BACKUP_DIR="${BASE_DIR}/backup"
 SRC_DIR="${BASE_DIR}/src"
 INST_DIR="${BASE_DIR}/install"
 LOCAL_DIR="${BASE_DIR}/local"
@@ -27,57 +35,87 @@ ROOT_FS="${FCOS_URI}/fedora-coreos-${FCOS_RELEASE}-live-rootfs.x86_64.img"
 RAW="${FCOS_URI}/fedora-coreos-${FCOS_RELEASE}-metal.x86_64.raw.xz"
 RAW_SIG="${FCOS_URI}/fedora-coreos-${FCOS_RELEASE}-metal.x86_64.raw.xz.sig"
 
-if [ -d ${SRC_DIR} ]; then
-    rm -rf ${SRC_DIR}
+_build_env()
+{
+printf "%s\n" "_build_env"
+[ -d ${INST_DIR} ] && rm -rf ${BACKUP_DIR}/* 
+[ -d ${INST_DIR} ] && mv -f ${INST_DIR} ${BACKUP_DIR}
+[ -d ${SRC_DIR} ] && mv -f ${SRC_DIR} ${BACKUP_DIR}
+mkdir -p ${SRC_DIR} ${PXE_OKD4_DIR} ${WWW_OKD4_DIR} ${INST_DIR}
+}
 
-    mkdir -p ${SRC_DIR} ${PXE_OKD4_DIR} ${WWW_OKD4_DIR}
+_download_and_copy()
+{
+printf "%s\n" "_download_and_copy"
+cd ${SRC_DIR}
 
-    cd ${SRC_DIR}
+printf "\t%s\n" "${CLIENT}"
+wget -q ${CLIENT}
+tar zxf $(echo ${CLIENT}| awk -F/ '{print $NF}')
 
-    wget -q ${CLIENT}
-    tar zxf $(echo ${CLIENT}| awk -F/ '{print $NF}')
+printf "\t%s\n" "${INSTALLER}"
+wget -q ${INSTALLER}
+tar zxf $(echo ${INSTALLER}| awk -F/ '{print $NF}')
 
-    wget -q ${INSTALLER}
-    tar zxf $(echo ${INSTALLER}| awk -F/ '{print $NF}')
+printf "\t%s\n" "${KERNEL}"
+wget -q ${KERNEL}
+cp $(echo ${KERNEL}| awk -F/ '{print $NF}') ${PXE_OKD4_DIR}/cos-kernel
 
-    wget -q ${KERNEL}
-    cp $(echo ${KERNEL}| awk -F/ '{print $NF}') ${PXE_OKD4_DIR}/cos-kernel
+printf "\t%s\n" "${INITRAMFS}"
+wget -q ${INITRAMFS}
+cp $(echo ${INITRAMFS}| awk -F/ '{print $NF}') ${PXE_OKD4_DIR}/cos-initramfs
 
-    wget -q ${INITRAMFS}
-    cp $(echo ${INITRAMFS}| awk -F/ '{print $NF}') ${PXE_OKD4_DIR}/cos-initramfs
+printf "\t%s\n" "${ROOT_FS}"
+wget -q ${ROOT_FS}
+cp $(echo ${ROOT_FS}| awk -F/ '{print $NF}') ${PXE_OKD4_DIR}/cos-live-rootfs
 
-    wget -q ${ROOT_FS}
-    cp $(echo ${ROOT_FS}| awk -F/ '{print $NF}') ${PXE_OKD4_DIR}/cos-live-rootfs
+printf "\t%s\n" "${RAW}"
+wget -q ${RAW}
+cp $(echo ${RAW}| awk -F/ '{print $NF}') ${WWW_OKD4_DIR}/cos.raw.xz
 
-    wget -q ${RAW}
-    cp $(echo ${RAW}| awk -F/ '{print $NF}') ${WWW_OKD4_DIR}/cos.raw.xz
+printf "\t%s\n" "${RAW_SIG}"
+wget -q ${RAW_SIG}
+cp $(echo ${RAW_SIG}| awk -F/ '{print $NF}') ${WWW_OKD4_DIR}/cos.raw.xz.sig
 
-    wget -q ${RAW_SIG}
-    cp $(echo ${RAW_SIG}| awk -F/ '{print $NF}') ${WWW_OKD4_DIR}/cos.raw.xz.sig
+chown -R apache:apache ${WEB_DIR}
+chmod -R 755 ${WEB_DIR}
 
-    chown -R apache:apache ${WEB_DIR}
-    chmod -R 755 ${WEB_DIR}
+cp openshift-install kubectl oc /usr/local/bin/
+}
 
-    cp openshift-install kubectl oc /usr/local/bin/
-fi
-
+_mk_install()
+{
+printf "%s\n" "_mk_install"
 if [ -f ${INSTALL_CONFIG} ]; then
-    rm -rf ${INST_DIR}
-    mkdir -p ${INST_DIR} 
     cd ${BASE_DIR}
 
     cp ${INSTALL_CONFIG} ${INST_DIR}/
 
+    printf "\t%s\n" "openshift-install create manifests --dir=${INST_DIR}/"
     openshift-install create manifests --dir=${INST_DIR}/
     sed -i 's/mastersSchedulable: true/mastersSchedulable: False/' ${INST_DIR}/manifests/cluster-scheduler-02-config.yml
+
+
+    printf "\t%s\n" "openshift-install create ignition-configs --dir=${INST_DIR}/"
     openshift-install create ignition-configs --dir=${INST_DIR}/
 
+    printf "\t%s\n" "cp -R ${INST_DIR}/auth/ ${LOCAL_DIR}/"
     cp -R ${INST_DIR}/auth/ ${LOCAL_DIR}/
 
     cd ${INST_DIR}
 
+    printf "\t%s\n" "cp -R * ${WWW_OKD4_DIR}/"
     cp -R * ${WWW_OKD4_DIR}/
 
     chown -R apache:apache ${WEB_DIR}
     chmod -R 755 ${WEB_DIR}
 fi
+}
+
+# main
+
+_build_env
+
+_download_and_copy
+
+_mk_install
